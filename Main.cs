c0dae316321b1c1cfa79ffe6077d49da19f6fdd0
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-04 00:14:03 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICreateBar
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "My Revit API";
+            application.CreateRibbonTab(tabName); //Создание вкладки
+            string utilsPath = @"C:\Program Files\RevitAPITraining\RevitAPIPipesWallsDoors.dll";
+            string utilsPath2 = @"C:\Program Files\RevitAPITraining\RevitAPIChangeWallType.dll";
+
+            var panel = application.CreateRibbonPanel(tabName, "Моя панель"); //Создание панели на вкладке
+
+            var button = new PushButtonData("Сведения", "Сбор сведений", utilsPath, "RevitAPIPipesWallsDoors.Main"); //Создание кнопки
+            var button2 = new PushButtonData("Изменения", "Изменить тип стен", utilsPath2, "RevitAPIChangeWallType.Main");
+
+            panel.AddItem(button); //Добавление кнопки на панель
+            panel.AddItem(button2);
+
+            return Result.Succeeded;
+        }
+    }
+}
